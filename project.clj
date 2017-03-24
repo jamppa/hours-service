bceca197b5e6c8668363c8f7e4cbbbbbcb4eb8b8
Jani Arvonen <jani.arvonen@gmail.com>
2017-03-24 16:27:10 +0200

    initial commit :)

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject hours-service "0.1.0-SNAPSHOT"
+  :description "Hours Service - service for handling commands and queries of hours app"
+  :url ""
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [compojure "1.5.1"]
+                 [org.apache.kafka/kafka-clients "0.10.2.0"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [cheshire "5.7.0"]
+                 [slingshot "0.12.2"]
+                 [com.novemberain/monger "3.1.0"]]
+  :main ^:skip-aot hours-service.core
+  :target-path "target/%s"
+  :profiles
+  {
+    :uberjar {:aot :all}
+    :dev {:dependencies [[midje "1.8.3"]]
+          :plugins [[lein-midje "3.2.1"]]}
+  })
---
+++ src/hours_service/core.clj
@@ -0,0 +1,5 @@
+(ns hours-service.core
+  (:gen-class))
+
+(defn -main [& args]
+  (println "this is hours service"))
