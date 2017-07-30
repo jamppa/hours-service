6fd81277d16ed0ff96141012f4f54982e7e8d0a6
Jani Arvonen <jani.arvonen@gmail.com>
2017-07-30 15:33:37 +0300

    customer: introduce created and creation failed -events

--- src/hours_service/customers/customer.clj
+++ src/hours_service/customers/customer.clj
@@ -4,7 +4,17 @@
 
 
 (defrecord Customer [_id name business-id])
+(defrecord CustomerCreated [type data])
+(defrecord CustomerCreationFailed [type data])
 
 
 (defn new [customer-map]
   (map->Customer (util/with-id customer-map)))
+
+
+(defn new-created [customer]
+  (->CustomerCreated "customer-created" customer))
+
+
+(defn new-creation-failed [customer]
+  (->CustomerCreationFailed "customer-creation-failed" customer))
--- test/hours_service/customers/customer_test.clj
+++ test/hours_service/customers/customer_test.clj
@@ -1,14 +1,27 @@
 (ns hours-service.customers.customer-test
   (:require
     [midje.sweet :refer :all]
-    [hours-service.customers.customer :as customer]))
+    [hours-service.customers.customer :as customer]
+    [hours-service.fixtures :as fixtures]))
 
 (def valid-customer-data {
   :name "The Firm Ltd"
   :business-id "1234567-8"})
 
-(fact "new valid customer has the id"
+
+(fact "new valid customer contains the id"
   (customer/new valid-customer-data) => (contains {:_id anything}))
 
-(fact "new valid customer has the data in it"
+
+(fact "new valid customer contains the data"
   (customer/new valid-customer-data) => (contains valid-customer-data))
+
+
+(fact "new created event contains type and the customer"
+  (customer/new-created fixtures/valid-customer) =>
+    (contains {:type "customer-created" :data fixtures/valid-customer}))
+
+
+(fact "new creation failed event contains the type and the customer"
+  (customer/new-creation-failed fixtures/valid-customer) =>
+    (contains {:type "customer-creation-failed" :data fixtures/valid-customer}))
