f51b3957a45778de006003029e567957b26a5507
Jani Arvonen <jani.arvonen@gmail.com>
2017-10-28 19:01:03 +0300

    chore(docker): rename service for clarity

--- run_development_shell.sh
+++ run_development_shell.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose -f docker/development.yml run --rm --service-ports --name hours-shell hours /bin/bash
+docker-compose -f docker/development.yml run --rm --service-ports --name hours-dev-shell hours-service /bin/bash
